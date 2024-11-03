27e2bf7eb174be21635f53a6f125415b6aca7a74
ht.pang <hantian.pang@gmail.com>
2024-11-03 11:12:02 +0800

    init main

---
+++ backend/main.py
@@ -0,0 +1,69 @@
+import uvicorn
+from fastapi import FastAPI
+from loguru import logger
+from psycopg_pool import ConnectionPool
+from pydantic import BaseModel
+
+from core.config import conf
+
+app = FastAPI()
+pool = ConnectionPool(
+    conninfo=(
+        f"host={conf.pg_host} "
+        f"port={conf.pg_port} "
+        f"dbname={conf.pg_dbname} "
+        f"user={conf.pg_user} "
+        f"password={conf.pg_password} "
+        f"sslmode=require"
+    ),
+    min_size=1,
+    max_size=10,
+)
+# URL_SUFFIX used to limit size of image
+URL_SUFFIX = "?fm=jpg&q=80&w=1080&h=1080&fit=max"
+
+
+def ping():
+    with pool.connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT 1")
+            logger.info(cur.fetchone())
+
+
+class Image(BaseModel):
+    id: str
+    url: str
+    category: str
+    description: str
+
+
+class ImagesResponse(BaseModel):
+    images: list[Image]
+
+
+@app.get("/api/image/random")
+def image_random(limit: int = 10) -> ImagesResponse:
+    resp = ImagesResponse(images=[])
+    with pool.connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT id,url,category,description FROM images ORDER BY RANDOM() LIMIT %s",
+                (limit,),
+            )
+            for row in cur.fetchall():
+                resp.images.append(
+                    Image(
+                        id=row[0],
+                        url=row[1] + URL_SUFFIX,
+                        category=row[2],
+                        description=row[3],
+                    )
+                )
+    return resp
+
+
+if __name__ == "__main__":
+    logger.info("Starting server...")
+    ping()
+    # run server
+    uvicorn.run(app, host="0.0.0.0", port=8000)

4eeba7110509750e6a39ff513f61401f2b615ae1
Design0 <design0webapp@gmail.com>
2024-12-30 22:29:27 +0800

    improve feature page

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -1,16 +1,5 @@
 import { CommonNavbar } from "@/components/common-navbar";
-import {
-  Clock,
-  CreditCard,
-  Edit,
-  MousePointer,
-  Search,
-  Smile,
-  Target,
-  Type,
-  Wand2,
-  Zap,
-} from "lucide-react";
+import { CreditCard, Smile, Target, Type, Wand2, Zap } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -39,13 +28,11 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 async function StepCard({
-  icon,
   image,
   title,
   description,
   emoji,
 }: {
-  icon: React.ReactNode;
   image: string;
   title: string;
   description: string;
@@ -54,13 +41,12 @@ async function StepCard({
   return (
     <Card className="text-center">
       <CardHeader>
-        <div className="flex justify-center mb-4">{icon}</div>
-        <CardTitle className="text-xl font-semibold mb-2">
-          {title} {emoji}
+        <CardTitle>
+          <h3>
+            {title} {emoji}
+          </h3>
         </CardTitle>
-        <CardDescription className="text-gray-600 mb-4">
-          {description}
-        </CardDescription>
+        <CardDescription>{description}</CardDescription>
       </CardHeader>
       <CardContent>
         <Image
@@ -68,10 +54,207 @@ async function StepCard({
           alt={title}
           width={400}
           height={300}
-          className="mx-auto"
+          className="w-full"
         />
       </CardContent>
     </Card>
+  );
+}
+
+async function HowToSection() {
+  return (
+    <section className="px-2 py-8 md:py-16 mx-auto w-[64rem] max-w-full">
+      <h2 className="text-3xl md:text-4xl font-semibold text-center mb-8">
+        How to Use Design0 🚀
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <StepCard
+          image="/features/features-1.webp"
+          title="Search & Choose"
+          description="Search for an image and select the perfect one for editing"
+          emoji="🔍"
+        />
+        <StepCard
+          image="/features/features-2.webp"
+          title="Mask Edit Area"
+          description="Click and drag to mask the area you want to edit"
+          emoji="🖱️"
+        />
+        <StepCard
+          image="/features/features-3.webp"
+          title="Write Prompt"
+          description="Describe what you want to edit in the masked area"
+          emoji="✏️"
+        />
+        <StepCard
+          image="/features/features-4.webp"
+          title="Get Results"
+          description="Wait for the magic to happen and see your edited image"
+          emoji="✨"
+        />
+      </div>
+    </section>
+  );
+}
+
+async function WhatIsSection() {
+  return (
+    <section className="py-8 md:py-16 mx-auto w-[64rem] max-w-full">
+      <h2 className="text-3xl md:text-4xl font-semibold text-center mb-8">
+        What is Design0 🤔
+      </h2>
+      <p className="px-2 text-gray-700 mt-2 text-justify">
+        As a non-professional designer, I frequently need to edit images for
+        posters and covers, which can be time-consuming. Finding images that
+        match my requirements is challenging, and adding visually appealing
+        text—beyond just using basic fonts—is particularly difficult. 🤕
+      </p>
+      <p className="px-2 text-gray-700 mt-2 text-justify">
+        After multiple attempts at learning professional design software, I
+        decided to build a tool for myself. 🎉
+      </p>
+    </section>
+  );
+}
+
+async function HowToWorkSection() {
+  return (
+    <section className="px-2 py-8 md:py-16 mx-auto w-[64rem] max-w-full">
+      <h2 className="text-3xl md:text-4xl font-semibold text-center mb-8">
+        How Does Design0 Work ⚙️
+      </h2>
+      <p className="px-2  text-gray-700 mt-2 text-justify">
+        Design0 integrates with{" "}
+        <Link
+          href="https://unsplash.com"
+          rel="nofollow"
+          className="underline"
+          target="_blank"
+        >
+          Unsplash
+        </Link>
+        , a platform offering a vast collection of free, high-resolution
+        photographs. It is a go-to resource for creatives who need quality
+        images for websites, presentations, social media, and design projects.
+      </p>
+      <Image
+        src={"/features/unsplash.webp"}
+        alt={"Unsplash introduction "}
+        width={800}
+        height={439}
+        className="mx-auto my-8 md:w-2/3 rounded-lg shadow-md"
+      />
+      <p className="px-2  text-gray-700 mt-2 text-justify">
+        Design0 goes beyond basic photo editing by offering advanced
+        capabilities. Users can edit specific areas of images with precision,
+        especially when it comes to creating stylized text effects. Through our
+        integration with{" "}
+        <Link
+          href="https://ideogram.ai/"
+          rel="nofollow"
+          className="underline"
+          target="_blank"
+        >
+          Ideogram
+        </Link>
+        , an innovative AI image generator, users can create text designs that
+        transcend conventional font limitations.
+      </p>
+    </section>
+  );
+}
+
+async function AchieveCard({
+  title,
+  prompt,
+  image1,
+  image2,
+}: {
+  title: string;
+  prompt: string;
+  image1: string;
+  image2: string;
+}) {
+  return (
+    <Card className="mt-6">
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>
+          <b>Prompt:</b> {prompt}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-6">
+          <Image
+            src={image1}
+            alt={`image before '${prompt}'`}
+            width={400}
+            height={400}
+            className="w-full rounded-lg shadow-md"
+          />
+          <Image
+            src={image2}
+            alt={`image after '${prompt}'`}
+            width={400}
+            height={400}
+            className="w-full rounded-lg shadow-md"
+          />
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+async function WhatCanAchieveSection() {
+  return (
+    <section className="px-2 py-8 md:py-16 mx-auto w-[64rem] max-w-full">
+      <h2 className="text-3xl md:text-4xl font-semibold text-center mb-8">
+        What Can You Achieve with Design0 🎯
+      </h2>
+      <p className="px-2  text-gray-700 mt-2 text-justify">
+        Whether you are working on social media content, marketing materials, or
+        personal projects, Design0 provides the tools you need to bring your
+        creative vision to life.
+      </p>
+      <h3 className="text-2xl md:text-3xl font-semibold text-center mt-8">
+        Modify Images Your Way
+      </h3>
+      <AchieveCard
+        title={"You can add custom elements based on your description"}
+        prompt={"add another bull is fighting"}
+        image1={"/features/add-elements-1.webp"}
+        image2={"/features/add-elements-2.webp"}
+      />
+      <AchieveCard
+        title={
+          "You can also transform existing elements in an image using prompts."
+        }
+        prompt={"change to house"}
+        image1={"/features/change-elements-1.webp"}
+        image2={"/features/change-elements-2.webp"}
+      />
+      <h3 className="text-2xl md:text-3xl font-semibold text-center mt-8">
+        Add Amazing Text to Images
+      </h3>
+      <AchieveCard
+        title={
+          "You can add sophisticated, visually appealing text limited only by your imagination."
+        }
+        prompt={
+          "add title with plant and flower: 'Planting Guide' in real style"
+        }
+        image1={"/features/add-text-1.webp"}
+        image2={"/features/add-text-2.webp"}
+      />
+      <AchieveCard
+        title={
+          "You can even create objects with custom text rendered onto them."
+        }
+        prompt={"add a flying balloon with text: 'Hope'"}
+        image1={"/features/object-with-text-1.webp"}
+        image2={"/features/object-with-text-2.webp"}
+      />
+    </section>
   );
 }
 
@@ -86,11 +269,11 @@ async function FeatureCard({
 }) {
   return (
     <Card>
-      <CardHeader>
-        <div className="flex items-center mb-4">
-          {icon}
-          <h3 className="text-xl font-semibold ml-2">{title}</h3>
-        </div>
+      <CardHeader className="flex flex-row items-center">
+        {icon}
+        <CardTitle className="text-xl font-semibold ml-2">
+          <h3>{title}</h3>
+        </CardTitle>
       </CardHeader>
       <CardContent>
         <p className="text-gray-600">{description}</p>
@@ -99,97 +282,77 @@ async function FeatureCard({
   );
 }
 
+async function FeaturesSection() {
+  return (
+    <section className="px-2 py-8 md:py-16 mx-auto w-[64rem] max-w-full">
+      <h2 className="text-3xl md:text-4xl font-semibold text-center mb-8">
+        Key Features 🌟
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <FeatureCard
+          icon={<Zap className="w-8 h-8 text-yellow-500" />}
+          title="Powerful Image Search"
+          description="Find the perfect image to start your creative journey"
+        />
+        <FeatureCard
+          icon={<Target className="w-8 h-8 text-red-500" />}
+          title="Precise Masked Editing"
+          description="Edit only the areas you want, leaving the rest untouched"
+        />
+        <FeatureCard
+          icon={<Wand2 className="w-8 h-8 text-blue-500" />}
+          title="Intelligent Prompt Editing"
+          description="Our AI understands and executes your editing instructions perfectly"
+        />
+        <FeatureCard
+          icon={<Smile className="w-8 h-8 text-orange-500" />}
+          title="User-Friendly Interface"
+          description="Intuitive design for seamless and enjoyable editing experience"
+        />
+        <FeatureCard
+          icon={<Type className="w-8 h-8 text-green-500" />}
+          title="Perfect Text Rendering"
+          description="Add or edit text with precision and style"
+        />
+        <FeatureCard
+          icon={<CreditCard className="w-8 h-8 text-purple-500" />}
+          title="Pay-as-you-go"
+          description="Only pay for what you use, no subscription required"
+        />
+      </div>
+    </section>
+  );
+}
+
 export default async function FeaturesPage() {
   return (
     <main className="min-h-screen flex flex-col">
       <CommonNavbar pageName={"Features"} />
       <section className="px-2 py-10 sm:py-12 md:py-16 xl:py-20 text-center bg-neutral-100">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">design0 🎨✨</h1>
-        <p className="text-xl md:text-2xl">Effortless Design for Everyone.</p>
-        <Button asChild={true} className="mt-6">
-          <Link href={"/"}>Try design0 Now! 🚀</Link>
+        <h1 className="text-4xl md:text-6xl font-bold tracking-tighter">
+          Design0 🎨
+        </h1>
+        <p className="mt-4 text-xl md:text-2xl">
+          Effortless Design for Everyone.
+        </p>
+        <Button variant="outline" asChild className="mt-6">
+          <Link href={"/"}>Try Design0 Now!</Link>
         </Button>
       </section>
 
-      <section className="py-4 md:py-8 mx-auto w-[64rem] max-w-full">
-        <h2 className="text-3xl md:text-4xl font-semibold text-center mb-8">
-          How to Use design0 🚀
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <StepCard
-            icon={<Search className="w-8 h-8" />}
-            image="/features/features-1.webp"
-            title="Search & Choose"
-            description="Search for an image and select the perfect one for editing"
-            emoji="🔍"
-          />
-          <StepCard
-            icon={<MousePointer className="w-8 h-8" />}
-            image="/features/features-2.webp"
-            title="Mask Edit Area"
-            description="Click and drag to mask the area you want to edit"
-            emoji="🖱️"
-          />
-          <StepCard
-            icon={<Edit className="w-8 h-8" />}
-            image="/features/features-3.webp"
-            title="Write Prompt"
-            description="Describe what you want to edit in the masked area"
-            emoji="✏️"
-          />
-          <StepCard
-            icon={<Clock className="w-8 h-8" />}
-            image="/features/features-4.webp"
-            title="Get Results"
-            description="Wait for the magic to happen and see your edited image"
-            emoji="✨"
-          />
-        </div>
-      </section>
-
-      <Separator className={"my-8"} />
-
-      <section className="py-4 md:py-8 mx-auto w-[64rem] max-w-full">
-        <h2 className="text-3xl md:text-4xl font-semibold text-center mb-8">
-          Key Features 🌟
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <FeatureCard
-            icon={<Zap className="w-8 h-8 text-yellow-500" />}
-            title="Powerful Image Search"
-            description="Find the perfect image to start your creative journey"
-          />
-          <FeatureCard
-            icon={<Target className="w-8 h-8 text-red-500" />}
-            title="Precise Masked Editing"
-            description="Edit only the areas you want, leaving the rest untouched"
-          />
-          <FeatureCard
-            icon={<Wand2 className="w-8 h-8 text-blue-500" />}
-            title="Intelligent Prompt Editing"
-            description="Our AI understands and executes your editing instructions perfectly"
-          />
-          <FeatureCard
-            icon={<Smile className="w-8 h-8 text-orange-500" />}
-            title="User-Friendly Interface"
-            description="Intuitive design for seamless and enjoyable editing experience"
-          />
-          <FeatureCard
-            icon={<Type className="w-8 h-8 text-green-500" />}
-            title="Perfect Text Rendering"
-            description="Add or edit text with precision and style"
-          />
-          <FeatureCard
-            icon={<CreditCard className="w-8 h-8 text-purple-500" />}
-            title="Pay-as-you-go"
-            description="Only pay for what you use, no subscription required"
-          />
-        </div>
-      </section>
+      <HowToSection />
+      <Separator />
+      <WhatIsSection />
+      <Separator />
+      <WhatCanAchieveSection />
+      <Separator />
+      <HowToWorkSection />
+      <Separator />
+      <FeaturesSection />
 
       <section className="py-4 md:py-8 text-center">
-        <Button size="lg" asChild={true}>
-          <Link href={"/"}>Try design0 Now! 🚀</Link>
+        <Button size="lg" asChild>
+          <Link href={"/"}>Try Design0 Now!</Link>
         </Button>
       </section>
 
--- app/page.tsx
+++ app/page.tsx
@@ -30,13 +30,16 @@ export default async function HomePage({
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
       <CommonNavbar pageName={null} />
-      <div className="flex-1 px-2 py-24 mx-auto w-[64rem] max-w-full flex flex-col items-center">
+      <div className="flex-1 px-2 py-20 mx-auto w-[64rem] max-w-full flex flex-col items-center">
         <h1 className="text-4xl font-black">Welcome to Design0!</h1>
-        <h2 className="mt-3 text-md font-semibold mb-6">
+        <h2 className="mt-3 text-md font-semibold mb-16">
           Effortless Design for Everyone.
         </h2>
         <Search query={searchParams.query} />
-        <div className="mt-10 columns-1 sm:columns-2 lg:columns-3 gap-3">
+        <p className="mt-2 text-gray-400 font-bold text-sm">
+          Search And Choose Image to Edit
+        </p>
+        <div className="mt-8 columns-1 sm:columns-2 lg:columns-3 gap-3">
           {photos.map((photo) => (
             <ImageCard key={photo.id} photo={photo} />
           ))}
